d122166d898276b04cf5b3d6e1d1f805b8eb16c5
Alamgir kabir <alamgirkabir.dev@gmail.com>
2023-02-21 15:50:57 +0600

    header.css  added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import Products from './components/Products/Products';
 
 function App() {
   const router = createBrowserRouter([
     { path: '/', element: <div> This is Default Page</div>},
-    { path: '/home', element: <Home></Home>},
+    { path: 'home', element: <Home></Home>},
     { path: 'about', element: <About></About>},
+    { path: 'products', element: <Products></Products>}
   ])
   return (
     <div className="App">
---
+++ src/components/Products/Products.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Products = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h5>This is products</h5>
+        </div>
+    );
+};
+
+export default Products;
