feff19e26b2cc593129f7dcbaba4fcbc9aec1b2c
Alamgir kabir <alamgirkabir.dev@gmail.com>
2023-02-21 15:18:14 +0600

    home,about, header  added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
 
 function App() {
   const router = createBrowserRouter([
     { path: '/', element: <div> This is Default Page</div>},
-    { path: '/home', element: <div> Home Page</div>},
-    { path: 'about', element: <div> This is About Page</div>}
+    { path: '/home', element: <Home></Home>},
+    { path: 'about', element: <About></About>},
   ])
   return (
     <div className="App">
---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const About = () => {
+    return (
+        <div>
+            <Header></Header>
+           <h3>This is About component</h3> 
+        </div>
+    );
+};
+
+export default About;<h3>This is About component</h3>
