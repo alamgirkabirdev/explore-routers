b96385a8551f4247628b841a203c7a294bdb4d99
Alamgir kabir <alamgirkabir.dev@gmail.com>
2023-02-22 16:27:21 +0600

    friends rout  added

--- src/App.js
+++ src/App.js
@@ -15,10 +15,16 @@ function App() {
         { path: '/', element: <Home></Home>},
       { path: 'home', element: <Home></Home>},
       { path: 'products', element: <Products></Products>},
-      { path: 'friends', element: <Friends></Friends>}
+      {
+        path: 'friends',
+        loader: async ()=> {
+          return fetch('http://jsonplaceholder.typicode.com/users');
+      },
+       element: <Friends></Friends>}
     ]},
     
     { path: 'about', element: <About></About>},
+    { path: '*', element: <div>This route not found: 404</div>}
     
   ])
   return (
